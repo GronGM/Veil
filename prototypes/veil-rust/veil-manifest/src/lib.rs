ff3ccb1cd21a497f0ffdafeea02225b608057d8b
GronGM <stripjustin@gmail.com>
2026-04-26 10:46:20 +0300

    Add initial public Rust workspace skeleton

---
+++ prototypes/veil-rust/veil-manifest/src/lib.rs
@@ -0,0 +1,7 @@
+#![forbid(unsafe_code)]
+
+//! Manifest and provider input skeleton for Veil.
+
+/// Placeholder manifest type for early workspace scaffolding.
+#[derive(Debug, Clone, Default)]
+pub struct ProviderManifest;
