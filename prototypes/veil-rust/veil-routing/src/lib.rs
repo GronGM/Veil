3de97513271e2577c5e9cd18d8f7762a50a4dd2b
GronGM <stripjustin@gmail.com>
2026-04-26 10:46:37 +0300

    Add initial public Rust workspace skeleton

---
+++ prototypes/veil-rust/veil-routing/src/lib.rs
@@ -0,0 +1,7 @@
+#![forbid(unsafe_code)]
+
+//! Routing skeleton for Veil.
+
+/// Placeholder route selection result for early workspace scaffolding.
+#[derive(Debug, Clone, Default)]
+pub struct RouteSelection;
